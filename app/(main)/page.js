ea6a800cca14e19d4c9e1c310ed1c725435c8baa
Atharva <athrudev@gmail.com>
2026-04-03 16:57:52 +0530

    Added the profile button in the page

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import {Button} from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-   <div className="h-screen bg-red-50 flex flex-col gap-5 items-center justify-center">
-  <h1 className="text-3xl text-indigo-600">
-    This is Protected route
-  </h1>
-  <Button variant="destructive">Click Me</Button>
+   <div className="h-screen bg-red-50 flex flex-col gap-5 px-8 py-8">
+  <UserButton
+  afterSignOutUrl="/"
+/>
 </div>
   );
 }
